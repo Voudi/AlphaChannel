8337beebdb4412c9876b2dc7e2935dcfa63d4be5
Voudi <voudicool@web.de>
2024-08-04 16:47:10 +0200

    Compatibility and Coexistence with the original Browsingway

---
+++ Pictomatic/Services.cs
@@ -0,0 +1,40 @@
+﻿using Dalamud.Interface.Windowing;
+using Dalamud.IoC;
+using Dalamud.Plugin;
+using Dalamud.Plugin.Services;
+
+namespace Pictomatic;
+
+public class Services
+{
+	[PluginService]
+	// ReSharper disable once AutoPropertyCanBeMadeGetOnly.Local
+	public static ICommandManager CommandManager { get; private set; } = null!;
+
+	[PluginService]
+	// ReSharper disable once AutoPropertyCanBeMadeGetOnly.Local
+	public static IChatGui Chat { get; private set; } = null!;
+
+	[PluginService]
+	// ReSharper disable once AutoPropertyCanBeMadeGetOnly.Local
+	public static IPluginLog PluginLog { get; private set; } = null!;
+
+	[PluginService]
+	// ReSharper disable once AutoPropertyCanBeMadeGetOnly.Local
+	public static ITextureProvider TextureProvider { get; private set; } = null!;
+
+	[PluginService]
+	// ReSharper disable once AutoPropertyCanBeMadeGetOnly.Local
+	public static IDalamudPluginInterface PluginInterface { get; private set; } = null!;
+
+	[PluginService]
+	// ReSharper disable once AutoPropertyCanBeMadeGetOnly.Local
+	public static IFramework Framework { get; private set; } = null!;
+
+	[PluginService]
+	// ReSharper disable once AutoPropertyCanBeMadeGetOnly.Local
+	public static IClientState ClientState { get; set; } = null!;
+
+	[PluginService] 
+	public static IObjectTable ObjectTable { get; private set; } = null!;
+}
