b764a3af497eb5e1eba280aecb05edf36ceb670e
Voudi <voudicool@web.de>
2025-07-22 18:41:07 +0200

    Tweaked Testversion

--- AddgistTestversion.sh
+++ AddgistTestversion.sh
@@ -1,22 +1,34 @@
 #!/bin/bash
-GIST="ff56526cfcd0197a87d518e07cf1302f"
+GIST="77c661ec1597925c2856d649a0cfc2d8"
+GISTTEST="ff56526cfcd0197a87d518e07cf1302f"
 
 git clone https://gist.github.com/Voudi/$GIST.git
 
-cp AlphaChannel.zip $GIST/
 cp pluginmaster.json $GIST/pluginmaster.json
-cp AlphaChannel/images/icon.png $GIST/icon.png
 cd $GIST
 git show-ref 
-git add AlphaChannel.zip
 git add pluginmaster.json
-git add icon.png
 
 git commit -m "Adding Zip to Gist"
  
 git push origin main
 #If prompted, provide your Github credentials associated with the gist
 cd ..
+
+git clone https://gist.github.com/Voudi/$GISTTEST.git
+
+cp AlphaChannel.zip $GISTTEST/AlphaChannel.zip
+cd $GISTTEST
+git show-ref 
+git add AlphaChannel.zip
+
+git commit -m "Adding Zip to Gist"
+ 
+git push origin main
+
+cd ..
+
 rm -rf $GIST
+rm -rf $GISTTEST
 rm -rf AlphaChannel.zip
 rm -rf pluginmaster.json
